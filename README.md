a34be4376fcff90cc81cbc7b05456ee1def722d4
Thomas Asadurian <Tasadurian@gmail.com>
2016-03-21 20:20:46 -0700

    added Berries and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#poke-go
+
+A Go wrapper for the popular [Pokeapi](http://pokeapi.co/)!
---
+++ berries.go
@@ -0,0 +1,33 @@
+package poke
+
+// Berries are small fruits that can provide HP and status condition restoration,
+// stat enhancement, and even damage negation when eaten by Pokémon.
+type Berries struct {
+	Firmness struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"firmness"`
+	Flavors []struct {
+		Flavor struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"flavor"`
+		Potency int `json:"potency"`
+	} `json:"flavors"`
+	GrowthTime int `json:"growth_time"`
+	ID         int `json:"id"`
+	Item       struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"item"`
+	MaxHarvest       int    `json:"max_harvest"`
+	Name             string `json:"name"`
+	NaturalGiftPower int    `json:"natural_gift_power"`
+	NaturalGiftType  struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"natural_gift_type"`
+	Size        int `json:"size"`
+	Smoothness  int `json:"smoothness"`
+	SoilDryness int `json:"soil_dryness"`
+}
