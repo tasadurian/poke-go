4c9b250c905ed6859295f31ac874aec3563a5333
Thomas Asadurian <Tasadurian@gmail.com>
2016-04-11 19:50:55 -0700

    added structs for evolutions

---
+++ types/EvolutionTrigger.go
@@ -0,0 +1,18 @@
+package pokego
+
+// EvolutionTrigger
+type EvolutionTrigger struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	PokemonSpecies []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"pokemon_species"`
+}
